6923aff422a2fe2d8e22f5d5bbcbe0921c7bd443
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-02-24 12:51:09 +0100

    changes 24.02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const allSelectors = '.logo, .aside-link, .icon-cros';
+const elements = document.querySelectorAll(allSelectors);
+const burger = document.querySelector('.icon-burger');
+const phone = document.querySelector('.icon-phone');
+
+phone.addEventListener('mouseenter', () => {
+  document.querySelector('.phone-number').classList.remove('displey-none');
+});
+
+phone.addEventListener('mouseleave', () => {
+  document.querySelector('.phone-number').classList.add('displey-none');
+});
+
+burger.addEventListener('click', () => {
+  document.body.classList.add('no-scroll');
+});
+
+elements.forEach((el) => {
+  el.addEventListener('click', () => {
+    document.body.classList.remove('no-scroll');
+  });
+});
